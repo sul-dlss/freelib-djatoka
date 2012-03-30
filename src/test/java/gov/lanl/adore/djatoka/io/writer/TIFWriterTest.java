3a12e23f518f551f404e6a38322c9edd6be139bb
Kevin S. Clarke <ksclarke@gmail.com>
2012-03-30 17:49:14 -0400

    updated image file name in TIFWriterTest

--- src/test/java/gov/lanl/adore/djatoka/io/writer/TIFWriterTest.java
+++ src/test/java/gov/lanl/adore/djatoka/io/writer/TIFWriterTest.java
@@ -6,54 +6,54 @@ import gov.lanl.adore.djatoka.io.FormatIOException;
 import gov.lanl.adore.djatoka.io.reader.DjatokaReader;
 
 import java.awt.image.BufferedImage;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Iterator;
-
-import javax.imageio.ImageIO;
 
 import org.junit.Test;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TIFWriterTest {
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(TIFWriterTest.class);
+    private static final Logger LOGGER = LoggerFactory
+	    .getLogger(TIFWriterTest.class);
 
-	@Test
-	public void testWrite() {
-		File outFile = null;
+    private static final String TIF = "src/test/resources/images/ms0332_gra_21518.tiff";
 
-		try {
-			outFile = File.createTempFile("test1-", ".tif");
-			
-			String input = "src/test/resources/images/sample.tif";
-			BufferedImage bufImage = new DjatokaReader().open(input);
-			OutputStream outStream = new FileOutputStream(outFile);
-			TIFWriter tifWriter = new TIFWriter();
+    @Test
+    public void testWrite() {
+	File outFile = null;
 
-			tifWriter.write(bufImage, outStream);
-			outStream.close();
-		}
-		catch (IOException details) {
-			fail(details.getMessage());
-		}
-		catch (FormatIOException details) {
-			fail(details.getMessage());
-		}
-		finally {
-			if (outFile != null) {
-				outFile.delete();
-			}
-		}
+	try {
+	    outFile = File.createTempFile("test1-", ".tif");
+
+	    BufferedImage bufImage = new DjatokaReader().open(TIF);
+	    OutputStream outStream = new FileOutputStream(outFile);
+	    TIFWriter tifWriter = new TIFWriter();
+
+	    tifWriter.write(bufImage, outStream);
+	    outStream.close();
 	}
-
-	@Test
-	public void testSetWriterProperties() {
-		// fail("Not yet implemented");
+	catch (IOException details) {
+	    fail(details.getMessage());
 	}
+	catch (FormatIOException details) {
+	    fail(details.getMessage());
+	}
+	finally {
+	    if (outFile != null) {
+		outFile.delete();
+	    }
+	}
+    }
+
+    @Test
+    public void testSetWriterProperties() {
+	// fail("Not yet implemented");
+    }
 
 }
