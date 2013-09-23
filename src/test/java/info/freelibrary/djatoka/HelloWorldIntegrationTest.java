c38073eb18c223f41bc79c81b74452a7b71e9520
Kevin S. Clarke <ksclarke@gmail.com>
2013-09-23 12:24:03 -0400

    made jetty.port configurable

--- src/test/java/info/freelibrary/djatoka/HelloWorldIntegrationTest.java
+++ src/test/java/info/freelibrary/djatoka/HelloWorldIntegrationTest.java
@@ -4,6 +4,8 @@ package info.freelibrary.djatoka;
 import static org.junit.Assert.fail;
 import static org.junit.Assert.assertEquals;
 
+import info.freelibrary.util.StringUtils;
+
 import java.net.HttpURLConnection;
 
 import org.slf4j.LoggerFactory;
@@ -19,12 +21,21 @@ public class HelloWorldIntegrationTest {
             .getLogger(HelloWorldIntegrationTest.class);
 
     private static final String QUERY =
-            "http://localhost:8888/resolve?url_ver=Z39.88-2004&rft_id=http%3A%2F%2Fmemory.loc.gov%2Fgmd%2Fgmd433%2Fg4330%2Fg4330%2Fnp000066.jp2&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.format=image/jpeg&svc.level=1";
+            "http://localhost:{}/resolve?url_ver=Z39.88-2004&rft_id=http%3A%2F%2Fmemory.loc.gov%2Fgmd%2Fgmd433%2Fg4330%2Fg4330%2Fnp000066.jp2&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.format=image/jpeg&svc.level=1";
 
     @Test
     public void test() {
+        String jettyPort = System.getProperty("jetty.port");
+        
         try {
-            URL url = new URL(QUERY);
+            Integer.parseInt(jettyPort);
+        }
+        catch (NumberFormatException details) {
+            fail("jetty.port is not an integer: " + jettyPort);
+        }
+        
+        try {
+            URL url = new URL(StringUtils.format(QUERY, jettyPort));
             HttpURLConnection huc = (HttpURLConnection) url.openConnection();
 
             huc.connect();
