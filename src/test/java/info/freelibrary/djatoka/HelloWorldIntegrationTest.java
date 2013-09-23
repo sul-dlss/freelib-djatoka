62f368e02f12ce8bf89d8a4874476d51f0e98252
Kevin S. Clarke <ksclarke@gmail.com>
2013-09-23 12:07:12 -0400

    added first stab at an integration test

---
+++ src/test/java/info/freelibrary/djatoka/HelloWorldIntegrationTest.java
@@ -0,0 +1,43 @@
+
+package info.freelibrary.djatoka;
+
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+import java.net.HttpURLConnection;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import java.net.URL;
+
+import org.junit.Test;
+
+public class HelloWorldIntegrationTest {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(HelloWorldIntegrationTest.class);
+
+    private static final String QUERY =
+            "http://localhost:8888/resolve?url_ver=Z39.88-2004&rft_id=http%3A%2F%2Fmemory.loc.gov%2Fgmd%2Fgmd433%2Fg4330%2Fg4330%2Fnp000066.jp2&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.format=image/jpeg&svc.level=1";
+
+    @Test
+    public void test() {
+        try {
+            URL url = new URL(QUERY);
+            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+
+            huc.connect();
+
+            assertEquals(200, huc.getResponseCode());
+            assertEquals(9206, huc.getContentLength());
+        } catch (Exception details) {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Error connecting to djatoka server", details);
+            }
+
+            fail(details.getMessage());
+        }
+    }
+
+}
