ac6b8f617271994235472408e1cb29cb50c6cbfc
Kevin S. Clarke <ksclarke@gmail.com>
2014-03-16 23:54:10 -0400

    formatting tweaks

--- src/main/java/info/freelibrary/djatoka/view/IdentifierResolver.java
+++ src/main/java/info/freelibrary/djatoka/view/IdentifierResolver.java
@@ -1,23 +1,32 @@
 
 package info.freelibrary.djatoka.view;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import java.util.List;
-import java.util.Arrays;
+import javax.servlet.http.HttpServletResponse;
 
-import java.util.concurrent.CopyOnWriteArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import gov.lanl.adore.djatoka.openurl.DjatokaImageMigrator;
 import gov.lanl.adore.djatoka.openurl.IReferentMigrator;
 import gov.lanl.adore.djatoka.openurl.IReferentResolver;
 import gov.lanl.adore.djatoka.openurl.ResolverException;
-
 import gov.lanl.adore.djatoka.util.ImageRecord;
 
 import info.freelibrary.djatoka.Constants;
-
 import info.freelibrary.util.PairtreeObject;
 import info.freelibrary.util.PairtreeRoot;
 import info.freelibrary.util.PairtreeUtils;
@@ -25,31 +34,17 @@ import info.freelibrary.util.StringUtils;
 
 import info.openurl.oom.entities.Referent;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URLDecoder;
-import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class IdentifierResolver implements IReferentResolver, Constants {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(IdentifierResolver.class);
 
-    private IReferentMigrator myMigrator = new DjatokaImageMigrator();
+    private final IReferentMigrator myMigrator = new DjatokaImageMigrator();
 
     private Map<String, ImageRecord> myRemoteImages;
 
-    private List<String> myIngestSources = new CopyOnWriteArrayList<String>();
+    private final List<String> myIngestSources = new CopyOnWriteArrayList<String>();
 
-    private List<String> myIngestGuesses = new CopyOnWriteArrayList<String>();
+    private final List<String> myIngestGuesses = new CopyOnWriteArrayList<String>();
 
     private File myJP2Dir;
 
@@ -59,16 +54,16 @@ public class IdentifierResolver implements IReferentResolver, Constants {
      * @param aRequest An image request
      * @return An image record
      */
-    public ImageRecord getImageRecord(String aRequest) throws ResolverException {
+    @Override
+    public ImageRecord getImageRecord(final String aRequest) throws ResolverException {
         String decodedRequest;
         ImageRecord image;
 
         try {
             decodedRequest = URLDecoder.decode(aRequest, "UTF-8");
             decodedRequest = URLDecoder.decode(decodedRequest, "UTF-8");
-        } catch (UnsupportedEncodingException details) {
-            // Should not be possible; JVMs must support UTF-8
-            throw new RuntimeException(details);
+        } catch (final UnsupportedEncodingException details) {
+            throw new RuntimeException("JVM doesn't support UTF-8!!", details);
         }
 
         // Check to see if the image is resolvable from a remote source
@@ -81,9 +76,8 @@ public class IdentifierResolver implements IReferentResolver, Constants {
 
             try {
                 referent = parseReferent(decodedRequest);
-            } catch (UnsupportedEncodingException details) {
-                // Should not be possible; JVMs must support UTF-8
-                throw new RuntimeException(details);
+            } catch (final UnsupportedEncodingException details) {
+                throw new RuntimeException("JVM doesn't support UTF-8!!", details);
             }
 
             // Check and see if we've already put it in the Pairtree FS
@@ -100,8 +94,8 @@ public class IdentifierResolver implements IReferentResolver, Constants {
             // make one last ditch attempt to find it as a remote image...
             if (image == null) {
                 for (int index = 0; index < myIngestGuesses.size(); index++) {
-                    String urlPattern = myIngestGuesses.get(index);
-                    String url = StringUtils.format(urlPattern, decodedRequest);
+                    final String urlPattern = myIngestGuesses.get(index);
+                    final String url = StringUtils.format(urlPattern, decodedRequest);
 
                     if (LOGGER.isDebugEnabled()) {
                         LOGGER.debug("Trying to resolve using URL pattern: {}", url);
@@ -121,14 +115,16 @@ public class IdentifierResolver implements IReferentResolver, Constants {
      * @param aReferent A referent for the desired image
      * @return An image record
      */
-    public ImageRecord getImageRecord(Referent aReferent) throws ResolverException {
-        String id = ((URI) aReferent.getDescriptors()[0]).toASCIIString();
+    @Override
+    public ImageRecord getImageRecord(final Referent aReferent) throws ResolverException {
+        final String id = ((URI) aReferent.getDescriptors()[0]).toASCIIString();
         return getImageRecord(id);
     }
 
     /**
      * Gets the referent migrator for this resolver.
      */
+    @Override
     public IReferentMigrator getReferentMigrator() {
         return myMigrator;
     }
@@ -139,14 +135,18 @@ public class IdentifierResolver implements IReferentResolver, Constants {
      * @param aReferentID The ID of a requested referent
      * @return An HTTP status code
      */
-    public int getStatus(String aReferentID) {
-        if (myRemoteImages.get(aReferentID) != null || // TODO: reversed?
-                getCachedImage(aReferentID) != null) {
-            return HttpServletResponse.SC_OK;
-        } else if (myMigrator.getProcessingList().contains(aReferentID)) {
-            return HttpServletResponse.SC_ACCEPTED;
-        } else {
-            return HttpServletResponse.SC_NOT_FOUND;
+    @Override
+    public int getStatus(final String aReferentID) {
+        try {
+            if (getImageRecord(aReferentID) != null) {
+                return HttpServletResponse.SC_OK;
+            } else if (myMigrator.getProcessingList().contains(aReferentID)) {
+                return HttpServletResponse.SC_ACCEPTED;
+            } else {
+                return HttpServletResponse.SC_NOT_FOUND;
+            }
+        } catch (final ResolverException details) {
+            return HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
         }
     }
 
@@ -155,9 +155,10 @@ public class IdentifierResolver implements IReferentResolver, Constants {
      * 
      * @param aProps A supplied properties configuration
      */
-    public void setProperties(Properties aProps) throws ResolverException {
-        String sources = aProps.getProperty("djatoka.known.ingest.sources");
-        String guesses = aProps.getProperty("djatoka.known.ingest.guesses");
+    @Override
+    public void setProperties(final Properties aProps) throws ResolverException {
+        final String sources = aProps.getProperty("djatoka.known.ingest.sources");
+        final String guesses = aProps.getProperty("djatoka.known.ingest.guesses");
 
         myJP2Dir = new File(aProps.getProperty(JP2_DATA_DIR));
         myMigrator.setPairtreeRoot(myJP2Dir);
@@ -167,11 +168,11 @@ public class IdentifierResolver implements IReferentResolver, Constants {
         myIngestGuesses.addAll(Arrays.asList(guesses.split("\\s+")));
     }
 
-    private boolean isResolvableURI(String aReferentID) {
+    private boolean isResolvableURI(final String aReferentID) {
         return aReferentID.startsWith("http"); // keeping it simple
     }
 
-    private ImageRecord getCachedImage(String aReferentID) {
+    private ImageRecord getCachedImage(final String aReferentID) {
         ImageRecord image = null;
 
         if (LOGGER.isDebugEnabled()) {
@@ -179,10 +180,10 @@ public class IdentifierResolver implements IReferentResolver, Constants {
         }
 
         try {
-            PairtreeRoot pairtree = new PairtreeRoot(myJP2Dir);
-            PairtreeObject dir = pairtree.getObject(aReferentID);
-            String filename = PairtreeUtils.encodeID(aReferentID);
-            File file = new File(dir, filename);
+            final PairtreeRoot pairtree = new PairtreeRoot(myJP2Dir);
+            final PairtreeObject dir = pairtree.getObject(aReferentID);
+            final String filename = PairtreeUtils.encodeID(aReferentID);
+            final File file = new File(dir, filename);
 
             if (file.exists()) {
                 image = new ImageRecord();
@@ -195,7 +196,7 @@ public class IdentifierResolver implements IReferentResolver, Constants {
             } else if (LOGGER.isDebugEnabled()) {
                 LOGGER.debug("Failed to find a JP2 in Pairtree cache");
             }
-        } catch (IOException details) {
+        } catch (final IOException details) {
             LOGGER.error("Failed to load file from cache", details);
         }
 
@@ -206,11 +207,11 @@ public class IdentifierResolver implements IReferentResolver, Constants {
         return image;
     }
 
-    private ImageRecord getRemoteImage(String aReferent, String aURL) {
+    private ImageRecord getRemoteImage(final String aReferent, final String aURL) {
         ImageRecord image = null;
 
         try {
-            URI uri = new URI(aURL);
+            final URI uri = new URI(aURL);
             File imageFile;
 
             // Check to see if it's already in the processing queue
@@ -240,7 +241,7 @@ public class IdentifierResolver implements IReferentResolver, Constants {
             } else {
                 throw new ResolverException("An error occurred processing file: " + uri.toURL());
             }
-        } catch (Exception details) {
+        } catch (final Exception details) {
             LOGGER.error(StringUtils.format("Unable to access {} ({})", aReferent, details.getMessage()));
 
             return null;
@@ -253,12 +254,12 @@ public class IdentifierResolver implements IReferentResolver, Constants {
         return image;
     }
 
-    private String parseReferent(String aReferent) throws UnsupportedEncodingException {
+    private String parseReferent(final String aReferent) throws UnsupportedEncodingException {
         String referent = aReferent;
 
         for (int index = 0; index < myIngestSources.size(); index++) {
-            Pattern pattern = Pattern.compile(myIngestSources.get(index));
-            Matcher matcher = pattern.matcher(referent);
+            final Pattern pattern = Pattern.compile(myIngestSources.get(index));
+            final Matcher matcher = pattern.matcher(referent);
 
             // If we have a parsable ID, let's use that instead of URI
             if (matcher.matches() && matcher.groupCount() > 0) {
